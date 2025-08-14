4ace49241e4309c6152cf848641d57563e2dd11b
luiz-matoso <luizhmatoso1340@gmail.com>
2025-08-14 15:15:19 -0300

    feat: adiciona DTOs e EmailService para enviar os email's de verificação de conta

---
+++ backend/src/main/java/com/luizmatoso/mindzy/dto/LoginUserDTO.java
@@ -0,0 +1,13 @@
+package com.luizmatoso.mindzy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginUserDTO {
+
+    private String email;
+    private String password;
+
+}
---
+++ backend/src/main/java/com/luizmatoso/mindzy/dto/RegisterUserDTO.java
@@ -0,0 +1,14 @@
+package com.luizmatoso.mindzy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDTO {
+
+    private String email;
+    private String password;
+    private String username;
+
+}
