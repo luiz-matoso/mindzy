68d2bc848c5815cbb538b24cdd86ce3558f24cde
luiz-matoso <luizhmatoso1340@gmail.com>
2025-08-15 13:50:10 -0300

    fix: arruma o texto do EmailConfiguration onde era para ser smtp estava smpt

--- backend/src/main/java/com/luizmatoso/mindzy/config/EmailConfiguration.java
+++ backend/src/main/java/com/luizmatoso/mindzy/config/EmailConfiguration.java
@@ -25,7 +25,7 @@ public class EmailConfiguration {
         mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smpt");
+        props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
