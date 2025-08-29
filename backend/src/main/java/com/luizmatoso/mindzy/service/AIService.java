7da559c2a2fec13b249b222e625771a3b710535c
luiz-matoso <luizhmatoso1340@gmail.com>
2025-08-29 14:32:59 -0300

    remove: apaga FunController e prompts de piada e curiosidades

--- backend/src/main/java/com/luizmatoso/mindzy/service/AIService.java
+++ backend/src/main/java/com/luizmatoso/mindzy/service/AIService.java
@@ -13,19 +13,9 @@ public class AIService {
            As respostas devem ser detalhadas sobre o assunto solicitado.
            """;
 
-    private static final String SYSTEM_PROMPT_PIADAS = """
-            Você é um humorista bastante engraçado.
-            Você cria piadas em formato enumerado com PERGUNTA e RESPOSTA.
-            """;
-
     private static final String SYSTEM_PROMPT_CODIGOS = """
             Você é um professor programador que conhece sobre códigos Python.
             Você ajuda a explicar códigos e cria códigos com base no texto.
-            """;
-
-    private static final String SYSTEM_PROMPT_CURIOSIDADES = """
-            Você é um contador de histórias que conhece muitas curiosidades sobre pessoas públicas e temas.
-            Você responde sempre traz curiosidades interessantes e surpreendentes.
             """;
 
     public AIService(ChatClient.Builder chatClientBuilder){
@@ -37,9 +27,7 @@ public class AIService {
         var chatClient = chatClientBuilder.build();
 
         String systemPrompt = switch (systemPromptMode.toLowerCase()){
-            case "piadas" -> SYSTEM_PROMPT_PIADAS;
             case "codigos" -> SYSTEM_PROMPT_CODIGOS;
-            case "curiosidades" -> SYSTEM_PROMPT_CURIOSIDADES;
             default -> SYSTEM_PROMPT_ESTUDOS;
         };
 
